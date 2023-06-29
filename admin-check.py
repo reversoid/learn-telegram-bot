7fd59f60458e2d26740e44d0307d6eb44ac8f83d
George Kobunov <ancient.gekob@gmail.com>
2023-06-29 13:29:38 +0300

    Implement admin check

---
+++ admin-check.py
@@ -0,0 +1,49 @@
+from typing import Any
+from aiogram.filters import BaseFilter
+from aiogram.types import Message
+from aiogram import Bot, Dispatcher
+from aiogram.types import Message
+
+admin_ids: list[int] = [550107937]
+
+class IsAdmin(BaseFilter):
+    def __init__(self, admin_ids: list[int]) -> None:
+        self.admin_ids = admin_ids
+
+    async def __call__(self, message: Message) -> Any:
+        return message.from_user.id in self.admin_ids
+
+
+
+# Вместо BOT TOKEN HERE нужно вставить токен вашего бота,
+# полученный у @BotFather
+API_TOKEN: str = '5681787061:AAF7n_XVkofeyf1QrMGXeSXAlx6-1rYycV0'
+
+# Создаем объекты бота и диспетчера
+bot: Bot = Bot(token=API_TOKEN)
+dp: Dispatcher = Dispatcher()
+
+# Собственный фильтр, проверяющий юзера на админа
+class IsAdmin(BaseFilter):
+    def __init__(self, admin_ids: list[int]) -> None:
+        # В качестве параметра фильтр принимает список с целыми числами 
+        self.admin_ids = admin_ids
+
+    async def __call__(self, message: Message) -> bool:
+        return message.from_user.id in self.admin_ids
+
+
+# Этот хэндлер будет срабатывать, если апдейт от админа
+@dp.message(IsAdmin(admin_ids))
+async def answer_if_admins_update(message: Message):
+    await message.answer(text='Вы админ')
+
+
+# Этот хэндлер будет срабатывать, если апдейт не от админа
+@dp.message()
+async def answer_if_not_admins_update(message: Message):
+    await message.answer(text=f'Вы не админ, ваш id {message.from_user.id}')
+
+
+if __name__ == '__main__':
+    dp.run_polling(bot)
